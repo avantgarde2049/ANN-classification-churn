101d609f0455c18bb92f151947667ddde8c854f7
Sagnik <57508800+avantgarde2049@users.noreply.github.com>
2026-09-04 17:33:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,19 @@ with open('scaler.pkl','rb') as file:
 ##streamlit app
 st.title('Customer churm prediction')
 
+# User input
+geography = st.selectbox('Geography', onehot_encoder_geo.categories_[0])
+gender = st.selectbox('Gender', label_encoder_gender.classes_)
+age = st.slider('Age', 18, 92)
+balance = st.number_input('Balance')
+credit_score = st.number_input('Credit Score')
+estimated_salary = st.number_input('Estimated Salary')
+tenure = st.slider('Tenure', 0, 10)
+num_of_products = st.slider('Number of Products', 1, 4)
+has_cr_card = st.selectbox('Has Credit Card', [0, 1])
+is_active_member = st.selectbox('Is Active Member', [0, 1])
+
+
 # Prepare the input data
 input_data = pd.DataFrame({
     'CreditScore': [credit_score],
